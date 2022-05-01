3b847225acb0de13cfd3d47c4b560da45912050a
LilTeker <48915085+LilTeker@users.noreply.github.com>
2022-05-01 14:05:03 +0200

    Progress on the database querys on msg event
    Temporarily moved getQuote() from db.js to andyBot.js, need to fix the promise problem on line 26 (https://discord.com/channels/102860784329052160/565213527673929729/970288707380858961)

--- andyBot.js
+++ andyBot.js
@@ -1,10 +1,8 @@
 // Load .env variables on this file
 require("dotenv").config();
-const db = require("./db.js");
-console.log(db);
-
-//
+const conn = require("./db.js");
 const { Client, Intents } = require("discord.js");
+
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
@@ -21,12 +19,30 @@ client.login(process.env.DISCORD_TOKEN);
 
 //Funcionality
 
-client.on("messageCreate", (msg) => {
+client.on("messageCreate", async (msg) => {
     if (msg.content == "que onda wacho") {
-        msg.reply("Practicando los pasos prohibidos weon");
-        console.log(`replied to a message from ${msg.author.username} id:${msg.author}`);
+        //msg.reply("Practicando los pasos prohibidos weon");
+        //console.log(`replied to a message from ${msg.author.username} id:${msg.author}`);
+        console.log(await getQuote(conn));
     }
 });
 
 
 //Load the quotes at start and print them from an array.
+
+
+
+
+async function getQuote(conn) {
+    let quote = await conn.query("SELECT * FROM quotes ORDER BY RAND() LIMIT 1", (err, result, fields) => {
+        if (err) throw err;
+        console.log(result[0].quote); //result es un "array" result[0].quote nos da el texto
+        return result;
+    });
+    /* let quote = await conn.query("SELECT * FROM quotes ORDER BY RAND() LIMIT 1");
+    return quote; */
+}
+
+
+
+//https://discord.com/channels/102860784329052160/565213527673929729/970288707380858961
--- db.js
+++ db.js
@@ -8,27 +8,12 @@ const conn = mysql.createConnection({
     database: process.env.DATABASE
 });
 
-/* function getQuote(conn) {
-    conn.query("SELECT * FROM quotes ORDER BY RAND() LIMIT 1", (err, result, fields) => {
-        if (err) throw err;
-        console.log(result); //result es un "array" result[0].quote nos da el texto
-    });
-} */
+
 
 conn.connect((err) => {
     if (err) throw err;
     console.log("connection successful");
-    getQuote();
 });
 
 
-
-//  err originates here 
-module.exports.getQuote = (conn) => {
-    conn.query("SELECT * FROM quotes ORDER BY RAND() LIMIT 1", (err, result, fields) => {
-        if (err) throw err;
-        console.log(result); //result es un "array" result[0].quote nos da el texto
-    });
-};
-
-module.exports.conn = conn;
+module.exports = conn;
