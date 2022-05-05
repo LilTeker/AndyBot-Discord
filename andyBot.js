a5b687530931d694c79eaa5a71fbd2c116a7973f
LilTeker <48915085+LilTeker@users.noreply.github.com>
2022-05-05 18:21:06 +0200

    Fixed the callback issue
    wrapped the callback query into a Promise object and resolved it on client.on event

--- andyBot.js
+++ andyBot.js
@@ -18,34 +18,40 @@ client.login(process.env.DISCORD_TOKEN);
 
 
 //Funcionality
-
-client.on("messageCreate", async (msg) => {
+client.on("messageCreate", (msg) => {
     if (msg.content == "que onda wacho") {
-        //msg.reply("Practicando los pasos prohibidos weon");
-        //console.log(`replied to a message from ${msg.author.username} id:${msg.author}`);
-        console.log(await getQuote(conn, (err, result, fields) => {
-            console.log(result[0].quote);
-        }));
+        let quote = getQuote(conn);
+        quote.then(
+            (result) => {
+                msg.reply(result[0].quote);
+                console.log(`replied to a message from ${msg.author.username} id:${msg.author}`);
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
     }
 });
 
 
-//Load the quotes at start and print them from an array.
+//Load the quotes at start and print them from an array. TO BE DONE
 
 
-
-
-async function getQuote(conn) {
-    let quote = await conn.query("SELECT * FROM quotes ORDER BY RAND() LIMIT 1", (err, result, fields) => {
-        if (err) throw err;
-        console.log(result[0].quote); //result es un "array" result[0].quote nos da el texto
-        return result;
+//function that uses mysql query callback based library and wraps it on a promise to pass the value to another function
+function getQuote(conn) {
+    let promise = new Promise(function (resolve, reject) {
+        conn.query("SELECT * FROM quotes ORDER BY RAND() LIMIT 1", (err, result, fields) => {
+            if (err) reject(err);
+            console.log(result[0].quote); //result es un "array" result[0].quote nos da el texto
+            resolve(result);
+        });
     });
-    /* let quote = await conn.query("SELECT * FROM quotes ORDER BY RAND() LIMIT 1");
-    return quote; */
+
+    return promise;
 }
 
 
 
 //https://discord.com/channels/102860784329052160/565213527673929729/970288707380858961
 //https://stackoverflow.com/questions/6847697/how-to-return-value-from-an-asynchronous-callback-function
+//https://www.includehelp.com/code-snippets/return-a-value-from-a-js-asynchronous-callback-function.aspx
